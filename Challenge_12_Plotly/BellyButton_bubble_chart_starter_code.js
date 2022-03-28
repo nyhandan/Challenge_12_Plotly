4185f5473246305466cf106b8e48a5cab9ae82ae
Dan Nyhan <93630983+nyhandan@users.noreply.github.com>
2022-03-27 19:03:28 -0500

    Add files via upload

---
+++ Challenge_12_Plotly/BellyButton_bubble_chart_starter_code.js
@@ -0,0 +1,42 @@
+// Bar and Bubble charts
+// Create the buildCharts function.
+function buildCharts(sample) {
+  // Use d3.json to load and retrieve the samples.json file 
+  d3.json("samples.json").then((data) => {
+    var samples= data.samples;
+    var resultsarray= samples.filter(sampleobject => sampleobject.id == sample);
+    var result= resultsarray[0]
+
+    var ids = result.otu_ids;
+    var labels = result.otu_labels;
+    var values = result.sample_values;
+
+    // Deliverable 1 Step 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot(); 
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [
+        {
+          x: ids,
+          y: values,
+          text: labels,
+          mode: "markers",
+          marker: {
+            color: ids,
+            size: values,
+            }
+        }
+     ];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      margin: { t: 0 },
+      xaxis: { title: "Id's" },
+      hovermode: "closest",
+    
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout)); 
+  });
+}
